b213f418e5d497beb34bcb3fa8d8caeff39bec8b
chavanak00-de <chavanak00@gmail.com>
2025-11-20 13:54:12 +0530

    code file

---
+++ Arithmatic operation in c.cpp
@@ -0,0 +1,57 @@
+	
+//Addition
+
+
+/*#include<stdio.h>
+int main(){
+	int a,b,sum;
+	printf("addition of two number");
+	scanf("%d %d",&a, &b);
+	sum=a+b;
+	printf("sum %d",sum);
+	return 0;
+}*/
+   //substraction
+   
+   
+	#include<stdio.h>
+	int main(){
+		int a,b,sub;
+		printf("substraction of two number");
+		scanf("%d %d",&a, &b);
+		sub=a-b;
+		printf("sum %d",sub);
+		return 0;
+	}
+
+   //Multiplication
+  /* #include<stdio.h>
+   int main(){
+int a,b,mul;
+	printf("multiplication of two number");
+	scanf("%d %d",&a,&b);
+	mul=a*b;
+	printf("mul %d",mul);
+	return 0;
+   }*/
+   
+   
+  // Division
+  /*#include<stdio.h>
+  int main(){
+  	int a,b,div;
+  	printf("division of two number");
+  	scanf("%d %d",&a,&b);
+  	div=a/b;
+  	printf("div %d",div);
+  	return 0;
+  }*/
+
+
+                   
+ 
+
+
+
+
+
